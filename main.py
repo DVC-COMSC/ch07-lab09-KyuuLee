5aed22db8df44d8dbc91a52a9d9670a9be68fdf0
Kyuu <69904775+KyuuLee@users.noreply.github.com>
2022-09-28 22:17:19 -0700

    0

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 # ******************************
 # Make your Code
 # ******************************
+
+names = input('Enter 5 students names').split()
+
+for i in range(len(names)):
+	if i == 0 or minlen > len(names[i]):
+		minlen = len(names[i])
+		minidx = i
+	elif minlen == len(names[i]) and names[i] < names[minidx]:
+		minlen = len(names[i])
+		minidx = i
+
+	if i == 0 or maxlen < len(names[i]):
+		maxlen = len(names[i])
+		maxidx = i
+	elif maxlen == len(names[i]) and names[i] < names[maxidx]:
+		maxlen = len(names[i])
+		maxidx = i
+
+print (names[minidx], names[maxidx])
+
+	
